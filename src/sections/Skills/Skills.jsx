59c2c0150839987860be1054830536090317e807
Janewu <98897727+Janewu1128@users.noreply.github.com>
2025-01-05 12:39:47 -0800

    Update skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -11,28 +11,40 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node.js" />
-        <SkillList src={checkMarkIcon} skill="Next.js" />
-      </div>
-      <hr />
+
+      <h2 className={styles.categoryTitle}>Programming Languages</h2>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Java" />
         <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="HTML/CSS" />
         <SkillList src={checkMarkIcon} skill="SQL" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
       </div>
       <hr />
+
+      <h2 className={styles.categoryTitle}>Frameworks and Databases</h2>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
+        <SkillList src={checkMarkIcon} skill="Spring Boot" />
+        <SkillList src={checkMarkIcon} skill="Django" />
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Next.js" />
+        <SkillList src={checkMarkIcon} skill="REST" />
+        <SkillList src={checkMarkIcon} skill="MySQL, PostgreSQL, MongoDB, DynamoDB" />
+      </div>
+      <hr />
+
+      <h2 className={styles.categoryTitle}>Tools</h2>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="AWS" />
+        <SkillList src={checkMarkIcon} skill="Docker" />
         <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="JUnit" />
+        <SkillList src={checkMarkIcon} skill="CI/CD" />
+        <SkillList src={checkMarkIcon} skill="Postman" />
+        <SkillList src={checkMarkIcon} skill="Jira" />
+        <SkillList src={checkMarkIcon} skill="Agile" />
       </div>
     </section>
   );
