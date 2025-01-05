55a40eed162a99e486bec8689acb20472bedc725
Janewu <98897727+Janewu1128@users.noreply.github.com>
2025-01-05 13:28:32 -0800

    update CV download name

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -53,7 +53,7 @@ function Hero() {
         <p className={styles.description}>
           Self-Driven Software Developer; Travel/Coffee Lover.
         </p>
-        <a href={CV} download>
+        <a href={CV} download="Nien-Chen(Jane)Wu-Resume.pdf">
           <button className="hover">Resume</button>
         </a>
       </div>
